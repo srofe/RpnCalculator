2a58f772d6314e4a0b629ca833dba9052b0febe3
Simon Rofe <simon.rofe@ieee.org>
2023-06-12 17:11:35 +1000

    Verify the item is added to the top of the stack
    YouTrack Issue: ^PCalc-2

--- tests/StackTests.cc
+++ tests/StackTests.cc
@@ -13,5 +13,11 @@ public:
 TEST(StackTests, PushItemToStackResultsInOneItemOnStack) {
     Stack stack;
     stack.push(3.2);
-    ASSERT_EQ(stack.content.size(), 1);
+    ASSERT_EQ(stack.content.size(), 1) << "Pushing an item to the stack shall create a container with one item.";
+}
+
+TEST(StackTests, PushItemToStackAddsThatItem) {
+    Stack stack;
+    stack.push(4.5);
+    ASSERT_EQ(stack.content[0], 4.5) << "Pushing an item to the stack shall add that item to the top of the container.";
 }
