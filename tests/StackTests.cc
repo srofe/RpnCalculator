f879cab3bea2ec60412f663415cecdb40a6ea082
Simon Rofe <simon.rofe@ieee.org>
2023-06-12 17:20:31 +1000

    Pop an item from the stack
    This retrieves the item from the stack.
    
    YouTrack Issue: ^PCalc-2

--- tests/StackTests.cc
+++ tests/StackTests.cc
@@ -8,6 +8,10 @@ public:
     void push(double item) {
         content.push_front(item);
     }
+
+    double pop() {
+        return content[0];
+    }
 };
 
 class StackTests : public testing::Test {
@@ -23,4 +27,10 @@ TEST_F(StackTests, PushItemToStackResultsInOneItemOnStack) {
 TEST_F(StackTests, PushItemToStackAddsThatItem) {
     sut.push(4.5);
     ASSERT_EQ(sut.content[0], 4.5) << "Pushing an item to the stack shall add that item to the top of the container.";
+}
+
+TEST_F(StackTests, PopItemRetrievesItemFromStack) {
+    sut.push(2.1);
+    double item = sut.pop();
+    ASSERT_EQ(item, 2.1) << "Popping an item from the stack shall retrieve that item from the stack.";
 }
