dd72c4d142c25f021f96cc58111ff050aec227d8
Simon Rofe <simon.rofe@ieee.org>
2023-07-03 14:30:20 +1000

    Pass message by reference
    YouTrack Issue: ^PCalc-3

--- applications/common/Publisher.h
+++ applications/common/Publisher.h
@@ -5,7 +5,7 @@
 
 class Exception : std::exception {
 public:
-    explicit Exception(const std::string message) : exceptionMessage(message) {}
+    explicit Exception(const std::string& message) : exceptionMessage(message) {}
     explicit Exception(const char* message) : exceptionMessage(message) {}
     ~Exception() noexcept override = default;
     const char * what() const noexcept override { return exceptionMessage.c_str(); }
