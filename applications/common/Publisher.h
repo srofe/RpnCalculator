45eee4ff3ce25e53ee725693719830a56ce9369e
Simon Rofe <simon.rofe@ieee.org>
2023-07-03 14:42:51 +1000

    Move Exception class to its own file
    YouTrack Issue: ^PCalc-3

--- applications/common/Publisher.h
+++ applications/common/Publisher.h
@@ -2,17 +2,7 @@
 
 #include <list>
 #include <Observer.h>
-
-class Exception : std::exception {
-public:
-    explicit Exception(const std::string& message) : exceptionMessage(message) {}
-    explicit Exception(const char* message) : exceptionMessage(message) {}
-    ~Exception() noexcept override = default;
-    const char * what() const noexcept override { return exceptionMessage.c_str(); }
-
-private:
-    std::string exceptionMessage;
-};
+#include <Exception.h>
 
 class Publisher {
 public:
