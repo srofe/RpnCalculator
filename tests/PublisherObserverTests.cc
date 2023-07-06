b5ea02e3c2dc3b6a514216accf0e6ad269d9481b
Simon Rofe <simon.rofe@ieee.org>
2023-07-06 15:42:02 +1000

    Use test fixture to simplify test
    Use a test fixture to create the sut (System Under Test), and use this
    in each test case instead of creating a MockPublisher.
    
    YouTrack Issue: ^PCalc-3

--- tests/PublisherObserverTests.cc
+++ tests/PublisherObserverTests.cc
@@ -17,45 +17,42 @@ public:
 
 MockObserver::MockObserver(std::string_view name) : Observer(name) {}
 
+class PublisherObserverTests : public testing::Test {
+public:
+    MockPublisher sut;
+};
+
 TEST(Observer, ObserverHasName) {
     MockObserver mockObserver("observer");
     ASSERT_EQ(mockObserver.name(), "observer") << "An Observer shall have a name.";
 }
 
-TEST(PublisherObserver, AttachingSubscriberToPublisherIncrementsSubscriberCount) {
+TEST_F(PublisherObserverTests, AttachingSubscriberToPublisherIncrementsSubscriberCount) {
     MockObserver mockObserver { "observer" };
-    MockPublisher mockPublisher;
-
-    mockPublisher.attach(&mockObserver);
-
-    ASSERT_EQ(mockPublisher.observerCount(), 1) << "Attaching an Observer to a Publisher shall increase the observers count by one.";
+    sut.attach(&mockObserver);
+    ASSERT_EQ(sut.observerCount(), 1) << "Attaching an Observer to a Publisher shall increase the observers count by one.";
 }
 
-TEST(PublisherObserver, DetatchingSubscribberDecrementsSubscriberCount) {
+TEST_F(PublisherObserverTests, DetatchingSubscribberDecrementsSubscriberCount) {
     MockObserver mockObserver1 { "observer1" };
     MockObserver mockObserver2 { "observer2" };
-    MockPublisher mockPublisher;
-
-    mockPublisher.attach(&mockObserver1);
-    mockPublisher.attach(&mockObserver2);
-    ASSERT_EQ(mockPublisher.observerCount(), 2) << "Confirm there are two observers attached.";
-    mockPublisher.detach("observer2");
-
-    ASSERT_EQ(mockPublisher.observerCount(), 1) << "Detaching an Observer from a Publisher shall decrease the observers count by one.";
+    sut.attach(&mockObserver1);
+    sut.attach(&mockObserver2);
+    ASSERT_EQ(sut.observerCount(), 2) << "Confirm there are two observers attached.";
+    sut.detach("observer2");
+    ASSERT_EQ(sut.observerCount(), 1) << "Detaching an Observer from a Publisher shall decrease the observers count by one.";
 }
 
-TEST(PublisherObserver, DetatchingUnknownObserverThrowsException) {
-    MockPublisher mockPublisher;
-    ASSERT_ANY_THROW(mockPublisher.detach("observer")) << "Attempting to detach an Observer that has not been attached to a Publisher shall throw an exception.";
+TEST_F(PublisherObserverTests, DetatchingUnknownObserverThrowsException) {
+    ASSERT_ANY_THROW(sut.detach("observer")) << "Attempting to detach an Observer that has not been attached to a Publisher shall throw an exception.";
 }
 
-TEST(PublisherObserver, CallingPublisherNotifyUpdatesAllObservers) {
+TEST_F(PublisherObserverTests, CallingPublisherNotifyUpdatesAllObservers) {
     MockObserver mockObserver1 { "Observer1" };
     MockObserver mockObserver2 { "Observer2" };
-    MockPublisher mockPublisher;
-    mockPublisher.attach(&mockObserver1);
-    mockPublisher.attach(&mockObserver2);
-    mockPublisher.notify();
+    sut.attach(&mockObserver1);
+    sut.attach(&mockObserver2);
+    sut.notify();
     ASSERT_TRUE(mockObserver1.updateCalled) << "The Publisher notify() method shall notify an Observer by calling its update() method - observer1.";
     ASSERT_TRUE(mockObserver2.updateCalled) << "The Publisher notify() method shall notify an Observer by calling its update() method - observer2.";
 }
