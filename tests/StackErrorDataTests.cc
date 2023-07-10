6cb91ee9f3812169845528546235fe7bb7c40420
Simon Rofe <simon.rofe@ieee.org>
2023-07-10 13:03:16 +1000

    Add new error condition
    YouTrack Issue: ^PCalc-3

--- tests/StackErrorDataTests.cc
+++ tests/StackErrorDataTests.cc
@@ -1,7 +1,8 @@
 #include <gmock/gmock.h>
 
 enum class ErrorCondition {
-    Empty
+    Empty,
+    TooFewArguments
 };
 
 class StackErrorData {
@@ -17,4 +18,9 @@ private:
 TEST(StackErrorDataTests, StackErrorDataHasErrorConditionEmpty) {
     StackErrorData errorData { ErrorCondition::Empty };
     ASSERT_EQ(errorData.error(), ErrorCondition::Empty) << "The StackErrorData shall have an error condition of Empty.";
+}
+
+TEST(StackeErrorDataTests, StackErrorDataHasErrorConditionTooFewArguments) {
+    StackErrorData errorData { ErrorCondition::TooFewArguments };
+    ASSERT_EQ(errorData.error(), ErrorCondition::TooFewArguments) << "The StackErrorData shall have an error condition of TooFewArguments.";
 }
