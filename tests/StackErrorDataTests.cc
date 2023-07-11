74f6d3a67249d8195777b2a682c87a36d15e465a
Simon Rofe <simon.rofe@ieee.org>
2023-07-11 11:59:58 +1000

    Add messages for different errors
    To test the default message, use a mock to expose the default
    constructor - which should be unavailable via the public API.
    
    While this default message should be unreachable (since the default
    constructor is protected), this ensures the message() function returns
    a string value regardless of the error condition.
    
    YouTrack Issue: ^PCalc-3

--- tests/StackErrorDataTests.cc
+++ tests/StackErrorDataTests.cc
@@ -8,11 +8,26 @@ enum class ErrorCondition {
 class StackErrorData {
 public:
     StackErrorData(ErrorCondition condition) : errorCondition(condition) {};
-    ErrorCondition error() { return errorCondition; }
+    inline ErrorCondition error() { return errorCondition; }
+    std::string message();
 
+protected:
+    StackErrorData() = default;
 private:
-    StackErrorData();
     ErrorCondition errorCondition;
+};
+
+std::string StackErrorData::message() {
+    switch (errorCondition) {
+        case ErrorCondition::Empty: return "Attempting to pop from empty stack.";
+        case ErrorCondition::TooFewArguments: return "At least two stack elements required for this operation.";
+    }
+    return "Unknown error.";
+}
+
+class StackErrorDataMock : public StackErrorData {
+public:
+    using StackErrorData::StackErrorData;
 };
 
 TEST(StackErrorDataTests, StackErrorDataHasErrorConditionEmpty) {
@@ -23,4 +38,19 @@ TEST(StackErrorDataTests, StackErrorDataHasErrorConditionEmpty) {
 TEST(StackErrorDataTests, StackErrorDataHasErrorConditionTooFewArguments) {
     StackErrorData errorData { ErrorCondition::TooFewArguments };
     ASSERT_EQ(errorData.error(), ErrorCondition::TooFewArguments) << "The StackErrorData shall have an error condition of TooFewArguments.";
+}
+
+TEST(StackErrorDataTests, StackErrorDataProvidesEmptyErrorMessage) {
+    StackErrorData errorData { ErrorCondition::Empty };
+    ASSERT_EQ(errorData.message(), "Attempting to pop from empty stack.") << "The StackErrorData shall provide a message for an error condition.";
+}
+
+TEST(StackErrorDataTests, StackErrorDataProvidesTooFewArgumentsMessage) {
+    StackErrorData errorData { ErrorCondition::TooFewArguments };
+    ASSERT_EQ(errorData.message(), "At least two stack elements required for this operation.") << "The StackErrorData shall provide a message for an error condition.";
+}
+
+TEST(StackErrorDataTests, Mock) {
+    StackErrorDataMock mockErrorData;
+    ASSERT_EQ(mockErrorData.message(), "Unknown error.");
 }
